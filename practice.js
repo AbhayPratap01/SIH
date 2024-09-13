0518364a50b33a66e2b016b25bd5a0c2031d5c3d
AbhayRathore2006 <103251542+AbhayPratap01@users.noreply.github.com>
2024-09-13 19:51:54 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,8 @@
+// JavaScript for login and sign-up buttons (placeholder)
+document.querySelector('.login-btn').addEventListener('click', () => {
+    alert('Login functionality coming soon!');
+});
+
+document.querySelector('.signup-btn').addEventListener('click', () => {
+    alert('Sign-up functionality coming soon!');
+});
